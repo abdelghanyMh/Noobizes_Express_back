1c24f38590a0243df96d398b06821a5662156694
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2024-09-06 15:28:46 +0100

    feat: get player info form gamename and tagline

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,62 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
+const axios = require("axios");
+
+const API_KEY = "RGAPI-74c49b71-9493-41c7-b9ac-95b22e5bd50d";
 
 // Define your GraphQL schema
 const typeDefs = gql`
+  type PlayerData {
+    id: String!
+    gameName: String!
+    tagLine: String!
+    profileIcon: String!
+    summonerLevel: Int!
+  }
+
   type Query {
-    hello: String
+    getPlayerData(gameName: String!, tagLine: String!): PlayerData
   }
 `;
 
-// Define your resolvers
+// Implement the resolvers
 const resolvers = {
   Query: {
-    hello: () => "Hello, World!",
+    getPlayerData: async (
+      _: any,
+      { gameName, tagLine }: { gameName: string; tagLine: string }
+    ) => {
+      try {
+        // First API request
+        const riotIdUrl = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${API_KEY}`;
+        const riotIdResponse = await axios.get(riotIdUrl);
+        const { puuid } = riotIdResponse.data;
+
+        if (!puuid) {
+          throw new Error("PUUID not found in the response");
+        }
+
+        // Second API request
+        const summonerUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`;
+        const summonerResponse = await axios.get(summonerUrl);
+        const { id, profileIconId, summonerLevel } = summonerResponse.data;
+
+        // Construct the profile icon URL
+        const profileIconUrl = `https://ddragon.leagueoflegends.com/cdn/14.17.1/img/profileicon/${profileIconId}.png`;
+
+        // Return the required data
+        return {
+          id,
+          gameName,
+          tagLine,
+          profileIcon: profileIconUrl,
+          summonerLevel,
+        };
+      } catch (error) {
+        console.error("Error:", error);
+        throw new Error("Failed to fetch player data");
+      }
+    },
   },
 };
 
